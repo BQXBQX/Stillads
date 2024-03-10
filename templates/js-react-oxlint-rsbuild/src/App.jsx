b3cb31c5bdbf0f58a3689f40f8dbf03daa68abcb
BQX <b22050106@njupt.edu.cn>
2024-03-10 14:45:25 +0800

    feat: js-react-oxlint-rsbuild #1

---
+++ templates/js-react-oxlint-rsbuild/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import React from "react";
+
+const App = () => {
+  var hello = djlkjasl
+
+  console.log("hello");
+  return (
+    <div className="content">
+      <h1>Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+    </div>
+  );
+};
+
+export default App;
